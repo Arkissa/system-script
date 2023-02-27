5cc56d1adb94f05a89ecf3733b175a5cbb906edb
molikatty <nicemolixi@gmail.com>
2023-02-27 21:42:48 +0800

    first commit

--- bin/translate/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-kill -9 \
-    "$(ps -ef | grep "/bin/python3 $HOME/scripts/bin/translate/translate.py" |\
-    sed 2d |\
-    awk '{print $2}')" && notify-send -r 9526 "󰊿 Close Translations" ||\
-    ~/scripts/bin/translate/translate.py
